9c095bf72d6d8d5ab7af6f4c3e953b58253810ed
MonkeSam <samuele.t.030204@gmail.com>
2024-11-26 17:11:24 +0100

    completed (teacher's errors while building)

--- 102-advanced-mvc/src/main/java/it/unibo/mvc/DrawNumberApp.java
+++ 102-advanced-mvc/src/main/java/it/unibo/mvc/DrawNumberApp.java
@@ -1,44 +1,60 @@
 package it.unibo.mvc;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 
 /**
  */
 public final class DrawNumberApp implements DrawNumberViewObserver {
-    private static final int MIN = 0;
-    private static final int MAX = 100;
-    private static final int ATTEMPTS = 10;
+
+    private static final String CONFIG_PATH = "src" + File.separator + "main"
+            + File.separator + "resources" + File.separator + "config.yml";
 
     private final DrawNumber model;
     private final List<DrawNumberView> views;
 
     /**
      * @param views
-     *            the views to attach
+     *              the views to attach
      */
     public DrawNumberApp(final DrawNumberView... views) {
         /*
          * Side-effect proof
          */
+        final int min;
+        final int max;
+        final int attempts;
+        try {
+            loadConfig();
+        } catch (IOException e) {
+            for (DrawNumberView drawNumberView : views) {
+                drawNumberView.displayError(e.getMessage());
+            }
+        }
         this.views = Arrays.asList(Arrays.copyOf(views, views.length));
-        for (final DrawNumberView view: views) {
+        for (final DrawNumberView view : views) {
             view.setObserver(this);
             view.start();
         }
-        this.model = new DrawNumberImpl(MIN, MAX, ATTEMPTS);
+        this.model = new DrawNumberImpl(min, max, attempts);
     }
 
     @Override
     public void newAttempt(final int n) {
         try {
             final DrawResult result = model.attempt(n);
-            for (final DrawNumberView view: views) {
+            for (final DrawNumberView view : views) {
                 view.result(result);
             }
         } catch (IllegalArgumentException e) {
-            for (final DrawNumberView view: views) {
+            for (final DrawNumberView view : views) {
                 view.numberIncorrect();
             }
         }
@@ -57,15 +73,31 @@ public final class DrawNumberApp implements DrawNumberViewObserver {
          * should be paid to alive threads, as the application would continue to persist
          * until the last thread terminates.
          */
-        System.exit(0);
+        System.exit(0);// NOPDM: on porpouse
+    }
+
+    /**
+     * Loads from config.yml attempts, minimum and maximum generated number.
+     * 
+     * @throws IOException
+     * @throws FileNotFoundException
+     */
+    public void loadConfig() throws FileNotFoundException, IOException {
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(new FileInputStream(CONFIG_PATH), StandardCharsets.UTF_8))) {
+            this.min = Integer.parseInt(reader.readLine().split(" ")[1]);
+            this.max = Integer.parseInt(reader.readLine().split(" ")[1]);
+            this.attempts = Integer.parseInt(reader.readLine().split(" ")[1]);
+        }
     }
 
     /**
      * @param args
-     *            ignored
-     * @throws FileNotFoundException 
+     *             ignored
+     * @throws FileNotFoundException
      */
     public static void main(final String... args) throws FileNotFoundException {
+        new DrawNumberApp(new DrawNumberViewImpl());
         new DrawNumberApp(new DrawNumberViewImpl());
     }
 
--- 102-advanced-mvc/src/main/java/it/unibo/mvc/DrawNumberView.java
+++ 102-advanced-mvc/src/main/java/it/unibo/mvc/DrawNumberView.java
@@ -11,7 +11,8 @@ public interface DrawNumberView {
     void setObserver(DrawNumberViewObserver observer);
 
     /**
-     * This method is called before the UI is used. It should finalize its status (if needed).
+     * This method is called before the UI is used. It should finalize its status
+     * (if needed).
      */
     void start();
 
@@ -24,4 +25,11 @@ public interface DrawNumberView {
      * @param res the result of the last draw
      */
     void result(DrawResult res);
+
+    /**
+     * Displays errors to the user via Dialog.
+     * 
+     * @param message
+     */
+    void displayError(String message);
 }
