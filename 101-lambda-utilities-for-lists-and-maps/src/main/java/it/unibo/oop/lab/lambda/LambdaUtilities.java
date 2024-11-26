534e07d20d64accb2f8b9c96dcd1d61bd84472a3
MonkeSam <samuele.t.030204@gmail.com>
2024-11-26 15:59:16 +0100

    completed

--- 101-lambda-utilities-for-lists-and-maps/src/main/java/it/unibo/oop/lab/lambda/LambdaUtilities.java
+++ 101-lambda-utilities-for-lists-and-maps/src/main/java/it/unibo/oop/lab/lambda/LambdaUtilities.java
@@ -1,11 +1,14 @@
 package it.unibo.oop.lab.lambda;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
@@ -14,12 +17,16 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 /**
- * This class will contain four utility functions on lists and maps, of which the first one is provided as example.
+ * This class will contain four utility functions on lists and maps, of which
+ * the first one is provided as example.
  * 
- * All such methods take as second argument a functional interface from the Java library (java.util.function).
- * This enables calling them by using the concise lambda syntax, as it's done in the main function.
+ * All such methods take as second argument a functional interface from the Java
+ * library (java.util.function).
+ * This enables calling them by using the concise lambda syntax, as it's done in
+ * the main function.
  *
- * Realize the three methods **WITHOUT** using the Stream library, but only leveraging the lambdas.
+ * Realize the three methods **WITHOUT** using the Stream library, but only
+ * leveraging the lambdas.
  *
  */
 public final class LambdaUtilities {
@@ -29,11 +36,11 @@ public final class LambdaUtilities {
 
     /**
      * @param list
-     *            the input list
+     *             the input list
      * @param op
-     *            the process to run on each element
+     *             the process to run on each element
      * @param <T>
-     *            element type
+     *             element type
      * @return a new list containing, for each element of list, the element and
      *         a processed version
      */
@@ -48,11 +55,11 @@ public final class LambdaUtilities {
 
     /**
      * @param list
-     *            input list
+     *             input list
      * @param pre
-     *            predicate to execute
+     *             predicate to execute
      * @param <T>
-     *            element type
+     *             element type
      * @return a list where each value is an Optional, holding the previous
      *         value only if the predicate passes, and an Empty optional
      *         otherwise.
@@ -61,18 +68,22 @@ public final class LambdaUtilities {
         /*
          * Suggestion: consider Optional.filter
          */
-        return null;
+        final List<Optional<T>> opts = new ArrayList<>();
+        list.forEach(x -> {
+            opts.add(Optional.of(x).filter(pre));
+        });
+        return opts;
     }
 
     /**
      * @param list
-     *            input list
+     *             input list
      * @param op
-     *            a function that, for each element, computes a key
+     *             a function that, for each element, computes a key
      * @param <T>
-     *            element type
+     *             element type
      * @param <R>
-     *            key type
+     *             key type
      * @return a map that groups into categories each element of the input list,
      *         based on the mapping done by the function
      */
@@ -80,7 +91,15 @@ public final class LambdaUtilities {
         /*
          * Suggestion: consider Map.merge
          */
-        return null;
+        final Map<R, Set<T>> map = new HashMap<>();
+
+        list.forEach(x -> {
+            map.merge(op.apply(x), new HashSet<>(Arrays.asList(x)), (a, b) -> {
+                a.addAll(b);
+                return a;
+            });
+        });
+        return map;
     }
 
     /**
@@ -101,12 +120,17 @@ public final class LambdaUtilities {
          *
          * Keep in mind that a map can be iterated through its forEach method
          */
-        return null;
+        final Map<K, V> filled = new HashMap<>();
+        map.forEach((x, y) -> {
+            filled.put(x, y.orElse(def.get()));
+        });
+
+        return filled;
     }
 
     /**
      * @param args
-     *            ignored
+     *             ignored
      */
     @SuppressWarnings("PMD.SystemPrintln")
     public static void main(final String[] args) {
